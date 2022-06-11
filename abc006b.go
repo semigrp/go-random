1382ca8888912ad2cf9d24d2d91452f1496d4b01
semigrp <semigroupoid@gmail.com>
2022-06-11 22:02:23 +0900

    wrw

--- abc006b.go
+++ abc006b.go
@@ -6,7 +6,7 @@ func main() {
 	var n int
 	fmt.Scan(&n)
 
-	var dp [n + 2]int
+	var dp [10007]int
 	dp[0] = 0
 	dp[1] = 0
 	dp[2] = 1
@@ -15,5 +15,5 @@ func main() {
 		dp[i] = dp[i-1] + dp[i-2]
 	}
 
-	fmt.Println(dp[n-1])
+	fmt.Println(dp[n-1] % 10007)
 }
